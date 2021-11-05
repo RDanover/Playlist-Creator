bd10742dce281074e7642062dec682cf4e337572
RDanover <danover.rachel@gmail.com>
2021-11-05 16:12:05 -0700

    Added function outlines and member declarations to header files

--- header/Playlist.hpp
+++ header/Playlist.hpp
@@ -1,11 +1,31 @@
 #ifndef PLAYLIST_HPP
 #define PLAYLIST_HPP
 
+#include <vector>
+#include <string>
+#include "Song.hpp"
+
+using namespace std;
+
 class Playlist {
     private:
-        
+	vector<Song*> songs;
+	vector<Song*> shuffled_songs;      
+	string name;
+	double length;  
     public:
-        
+        Playlist(string);
+        string get_playlist_name(){return name;}
+	double get_length(){return length;}
+	
+	void display_options();
+	void add_song();
+	void delete_song();
+	void hide_unhide_song();
+	void play();
+	void play_song();
+	void shuffle();
+	void analytics();
 };
 
 #endif // PLAYLIST_HPP
--- header/User_Menu.hpp
+++ header/User_Menu.hpp
@@ -1,10 +1,21 @@
 #ifndef USER_MENU_HPP
 #define USER_MENU_HPP
 
+#include <vector>
+#include <string>
+#include "Playlist.hpp"
+
+using namespace std;
+
 class User_Menu {
     private:
-        
+	vector<Playlist*> playlists;	        
     public:
+	void display_options();
+	void create_new_playlist();
+	void delete_playlist();
+	void access_playlist();
+	void exit();
 };
 
 #endif // USER_MENU_HPP
