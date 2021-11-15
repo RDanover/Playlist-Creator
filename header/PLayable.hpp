0e81ed4e3c8d7b4296a90b2d4c7b228fad3626e6
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-15 15:56:01 -0800

    Create PLayable.hpp

---
+++ header/PLayable.hpp
@@ -0,0 +1,13 @@
+#ifndef PLAYABLE_HPP
+#define PLAYABLE_HPP
+
+#include <string>
+
+using namespace std;
+
+class Playable {
+    public:
+       virtural string get_name()=0;
+       virtural double get_length()=0;
+};
+#endif //PLAYABLE_HPP
