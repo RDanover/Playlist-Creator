75e610c6b6f1afef2972b9ef7e455dfd224bede6
seokhakangcs <seokhakangcs@gmail.com>
2021-11-15 22:14:58 -0800

    Create Abstract Factory and sub-classes

---
+++ header/private_factory.hpp
@@ -0,0 +1,11 @@
+#ifndef __PRIVATE_FACTORY_HPP__
+#define __PRIVATE_FACTORY_HPP__
+#include "abs_factory.hpp"
+#include <string>
+
+class Private_Factory: public Abs_Factory {
+	virtual void create_playlist(){}
+	virtual void create_song(){}
+}
+
+#endif
