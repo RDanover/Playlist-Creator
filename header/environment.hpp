6df92102ab7d1072f061553afc8ed2151303350a
seokhakangcs <80696887+seokhakangcs@users.noreply.github.com>
2021-11-16 00:22:08 -0800

    Delete environment.hpp

--- header/environment.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __ENVIRONMENT__
-#define __ENVIRONMENT__
-#include <string>
-class Enviroment{
-    std::string environment;
-    virtual void create_environment(std::string new_environment){
-	this.environment = new_environment;
-}
-#endif
