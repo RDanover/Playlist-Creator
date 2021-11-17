4733762acf1da1a49d9948141ea56686f11e1763
RDanover <danover.rachel@gmail.com>
2021-11-17 15:25:16 -0800

    Simplified Hide_Unhide function

--- src/Playlist.cpp
+++ src/Playlist.cpp
@@ -23,7 +23,7 @@ void Playlist::hide_unhide_song(){
 	string songname = "";
 	string response = "";
 	int responseInt = 0;
-	int indexOfSong = 0;
+	int indexOfSong = -1;
 	cout<<"Please enter the name of the song you would like to hide or unhide, or enter LEAVE, to return to the main menu."<<endl;
 	cin.ignore();
 	getline(cin,songname);
@@ -34,92 +34,29 @@ void Playlist::hide_unhide_song(){
 		return;
 	}
 	else{
-		vector <int> indexes;
+
 		for(unsigned i<0;i<songs.size();i++){
 			if(songs.at(i)->get_name().equals(songname)){
-				indexes.pushback(i);
+				indexOfSong = i;	
 			}
 		}
-		if(indexes.size()<1){
+		if(indexOfSong==-1){
 			cout<<"Song not found. Please try again"<<endl;
 			hide_unhide_songs();
 			return;
 		}
-		else if(indexes.size() == 1){
-			if(songs.at(indexes.at(0)).get_hidden_status()){
-				cout << "Would you like to unhide "<< songname<<"? Enter Y for yes, enter N for no."<<endl;
-				cin >> response;
-				cin.ignore();
-				if(response.equals("Y"){
-					songs.at(indexes.at(0))->set_hidden_status();
-					cout<<songname<<" has been unhidden."<<endl;
-					return;
-				}
-				else{
-					cout<<"Unhide aborted."<<endl;
-					return;
-				}
+		else {
+			if(songs.at(indexOfSong)->get_hidden_status()){
+				songs.at(indexOfSong)->set_hidden_status();
+				cout<<songs.at(indexOfSong)->get_name()<<" Has been unhidden."<<endl;
 			}
 			else{
-				cout << "Would you like to hide "<<songname<<" Enter Y for yes, enter N for no."<<endl;
-				cin >>response;
-				cin.ignore();
-				if(response.equals("Y"){
-					songs.at(indexes.at(0))->set_hidden_status();
-					cout<<songname<<" had been hidden."<<endl;
-					return;
-				}
-				else{
-					cout<<"Hide aborted."<<endl;
-					return;
-				}
+				songs.at(indexOfSong)->set_hidden_status();
+				cout<<songs.at(indexOfSong)->get_name()<<" Has been hidden."<<endl;
 			}
-		}
-		else{
-			cout << "Multiple songs found with that same name:"<<endl;
-			for(int j =0;j<indexes.size();j++){
-				cout<<j+1<<". "<<songname<<" by "<<songs.at(indexes.at(j))->get_artist()<<endl;
-			}
-			cout << "Please enter the number that corresponds to the song you would like to hide or unhide. Or enter 0 to return to menu"<<endl;
-			cin>>responseInt;
-			cin.ignore();
-			if(responseInt==0||responseInt>indexes.size()+1){
-				cout<<"returning to main menu."
-				display_menu();
-				return;
-			}
-			else{
-				if(songs.at(indexes.at(responseInt-1))->get_hidden_status()){
-                                	cout << "Would you like to unhide "<< songname<<"? Enter Y for yes, enter N for no."<<endl;
-                                	cin >> response;
-                                	cin.ignore();
-                                	if(response.equals("Y"){
-                                 	       songs.at(indexes.at(0))->set_hidden_status();
-                                 	       cout<<songname<<" has been unhidden."<<endl;
-                                 	       return;
-                                	}
-                                	else{
-                                        	cout<<"Unhide aborted."<<endl;
-                                        	return;
-                                	}
-                        	}
-                        	else{
-                                	cout << "Would you like to hide "<<songname<<" Enter Y for yes, enter N for no."<<endl;
-                                	cin >>response;
-                                	cin.ignore();
-                                	if(response.equals("Y"){
-                                        	songs.at(indexes.at(0))->set_hidden_status();
-                                        	cout<<songname<<" had been hidden."<<endl;
-                                        	return;
-                                	}
-                                	else{
-                                        	cout<<"Hide aborted."<<endl;
-                                       		 return;
-                                	}
-                        	}
-			}
-				
-		}
+			return;
+		
+		}	
 	}
 }
 
