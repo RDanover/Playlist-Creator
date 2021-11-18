a5e3374104f6de03993aa2ec063c1eeb96aa4228
RDanover <danover.rachel@gmail.com>
2021-11-17 16:25:21 -0800

    Made shuffle truly randomize and handle certain edge cases - no songs have been added - the user has already called shuffle

--- src/Playlist.cpp
+++ src/Playlist.cpp
@@ -1,4 +1,7 @@
 #include "Playlist.hpp"
+#include <stdlib.h>
+#include <time.h>
+#include <iterator>
  
 Playlist::Playlist(string n){
 	name = n;
@@ -30,27 +33,35 @@ void Playlist::play_song(){
 }
 
 void Playlist::shuffle(){
-	if(songs->size()<1){
+	if(songs.size()<1){
 		cout<<"please add songs before trying to shuffle the plaaylist"<<endl;
 		return;
 	}
 	
-	song* temp;
-	for(int i=songs.size()-1;i>=0;i--){
-		shuffled_songs.push_back(songs.at(i));
-	}
+	shuffled_songs = songs;
+	
+	vector <Song*> temp;
+	temp = songs;
+	
+	vector<Song*>::iterator ptr;
+	
+	srand(time(NULL));//setting the seed based on the current time
+	
+	int randInt=0;
+	
+	for(int i =0; i<songs.size(); i++){
+		randInt = rand()%(temp.size());
+		ptr = temp.begin();
+		shuffled_songs.at(i) = temp.at(randInt);
+		ptr += randInt;
+		temp.erase(ptr);
+	}	
 
-	for(int j = 0;j< shuffled_songs.size()-1;j+=2){
-		temp = shuffled_songs.at(shuffled_songs.at(j+1);
-		shuffled_songs.at(j+1) = shuffled_songs.at(j);
-		shuffled_songs.at(j) = temp;
-	}
-	
-	cout<<"songs have been shuffled"<<endl;
-	
+	cout<<"songs have been shuffled"<<endl;	
 
 }
 
 void Playlist::analytics(){
 	//insert implementation
-}
+}shuffled_songs = temp;
+
