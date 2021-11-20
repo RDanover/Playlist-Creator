b3949ef8ecea53cb6b35abada47ba6706bea77fa
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-19 16:55:07 -0800

    Update Playable.hpp

--- header/Playable.hpp
+++ header/Playable.hpp
@@ -9,6 +9,21 @@ class Playable {
     public:
        virtual string get_name()=0;
        virtual double get_length()=0;
-       virtual void display_options(){}
+       virtual void display(){}
+       virtual void add_song(string,string,double){}
+       virtual void delete_song(){}
+       virtual void hide_unhide_song(){}
+       virtual void play(){}
+       virtual void play_song(){}
+       virtual void shuffle(){}
+       virtual void analytics(){}
+       virtual void display_songs{}
+       
+       virtual void get_artist(){}
+       virtual bool get_hidden_status(){}
+       virtual int get_num_time_played(){}
+       virtual void set_hidden_status(){}
+       virtual void increment_time_played(){}
+    
 };
 #endif //PLAYABLE_HPP
