1ba5f9f6051cd6f2303763213414b36a5f1ad038
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-28 21:38:29 -0800

    Create abs_song.hpp

---
+++ header/abs_song.hpp
@@ -0,0 +1,21 @@
+#ifndef ABS_SONG_HPP
+#define ABS_SONG_HPP
+
+#include "Playable.hpp"
+#include <string>
+
+using namespace std;
+
+class Abs_Song : public Playable
+{
+public:
+	virtual string get_name()= 0;
+	virtual string get_artist() = 0;
+	virtual double get_length() = 0; 
+	virtual bool get_hidden_status() = 0;
+	virtual int get_num_time_played() {return -1; }
+	virtual void set_hidden_status() = 0;
+	virtual void increment_time_played() {}
+};
+
+#endif // ABS_SONG_HPP
