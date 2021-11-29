1d76b233db30369749dc57d2f15c8da638d763b8
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-28 23:33:16 -0800

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -15,7 +15,7 @@ int main()
             
             cout<<"Test one: No playlists have been added"<<endl;
             User_Menu* access_playlist_test_one = new User_Menu();
-            int access_playlist_test_one_result = access_playlist_test_one->access_playlist();
+            int access_playlist_test_one_result = access_playlist_test_one->access_playlist(1);
             cout<<"Test Result: ";
             if(access_playlist_test_one_result == 1){
                 cout<<"PASSED: user was printed error message and was returned to options"<<endl;
@@ -25,6 +25,20 @@ int main()
             }
             cout<<"End test one."<<endl;
     
+        cout<<"Test two: check that all playlists are printed to the user"<<endl;
+            User_Menu* access_playlist_test_two = new User_Menu();
+            access_playlist_test_two->public_add_playlist();
+            access_playlist_test_two->public_add_playlist();
+            access_playlist_test_two->public_add_playlist();
+            int access_playlist_test_two_result = access_playlist_test_two->access_playlist(2);
+            cout<<"Test Result: ";
+            if(access_playlist_test_one_result == 3){
+                cout<<"PASSED: user was printed all playlists"<<endl;
+            }
+            else{
+                cout<<"FAILED: undefined behavior"<<endl;
+            }
+            cout<<"End test two."<<endl;
     
     
     cout<<"====================="<<endl;
