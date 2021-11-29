fd031d2b9cef9ae7c6221937d69a48dd3aee302e
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-28 23:45:45 -0800

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,11 +8,13 @@ int main()
     cout<<"====================="<<endl;
     cout<<"RUNNING PROGRAM TESTS"<<endl;
     cout<<"====================="<<endl;
+    cout<<endl;
     
         cout<<"====================="<<endl;
         cout<<"RUNNING ACCESS_PLAYLIST TESTS"<<endl;
         cout<<"====================="<<endl;
-            
+        cout<<endl;
+    
             cout<<"Test one: No playlists have been added"<<endl;
             User_Menu* access_playlist_test_one = new User_Menu();
             int access_playlist_test_one_result = access_playlist_test_one->access_playlist(1);
@@ -23,22 +25,23 @@ int main()
             else{
                 cout<<"FAILED: undefined behavior"<<endl;
             }
-            cout<<"End test one."<<endl;
+            cout<<endl;
     
             cout<<"Test two: check that all playlists are printed to the user"<<endl;
             User_Menu* access_playlist_test_two = new User_Menu();
-            access_playlist_test_two->add_playlist();
-            access_playlist_test_two->add_playlist();
-            access_playlist_test_two->add_playlist();
+            int access_playlist_test_two_num = 3;//change this number to change number of playlists
+            for(int access_playlist_test_two_num_i=0;access_playlist_test_two_num_i<=access_playlist_test_two_num;access_playlist_test_two_num_i++){
+                access_playlist_test_two->add_playlist();
+            }
             int access_playlist_test_two_result = access_playlist_test_two->access_playlist(2);
             cout<<"Test Result: ";
-            if(access_playlist_test_two_result == 3){
+            if(access_playlist_test_two_result == access_playlist_test_two_num){
                 cout<<"PASSED: user was printed all playlists"<<endl;
             }
             else{
                 cout<<"FAILED: undefined behavior"<<endl;
             }
-            cout<<"End test two."<<endl;
+            cout<<endl;
     
     
     cout<<"====================="<<endl;
