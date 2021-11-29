3db70b93f95dcb5f6515f61ee882b6480e1fd004
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-29 00:00:43 -0800

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -17,7 +17,7 @@ int main()
     
             cout<<"Test one: No playlists have been added"<<endl;
             User_Menu* access_playlist_test_one = new User_Menu();
-            int access_playlist_test_one_result = access_playlist_test_one->access_playlist(1);
+            int access_playlist_test_one_result = access_playlist_test_one->access_playlist(1,0);
             cout<<"Test Result: ";
             if(access_playlist_test_one_result == 1){
                 cout<<"PASSED: user was printed error message and was returned to options"<<endl;
@@ -33,7 +33,7 @@ int main()
             for(int access_playlist_test_two_num_i=0;access_playlist_test_two_num_i<access_playlist_test_two_num;access_playlist_test_two_num_i++){
                 access_playlist_test_two->add_playlist();
             }
-            int access_playlist_test_two_result = access_playlist_test_two->access_playlist(2);
+            int access_playlist_test_two_result = access_playlist_test_two->access_playlist(2,0);
             cout<<"Test Result: ";
             if(access_playlist_test_two_result == access_playlist_test_two_num){
                 cout<<"PASSED: user was printed all playlists"<<endl;
@@ -42,6 +42,30 @@ int main()
                 cout<<"FAILED: undefined behavior"<<endl;
             }
             cout<<endl;
+            
+            cout<<"Test three: if user inputs number too high or too low"<<endl;
+            User_Menu* access_playlist_test_three = new User_Menu();
+            int access_playlist_test_three_num = 3;//change this number to change number of playlists
+            for(int access_playlist_test_three_num_i=0;access_playlist_test_three_num_i<access_playlist_test_three_num;access_playlist_test_three_num_i++){
+                access_playlist_test_three->add_playlist();
+            }
+            int access_playlist_test_three_result_low = access_playlist_test_two->access_playlist(3,-1);
+            int access_playlist_test_three_result_high = access_playlist_test_two->access_playlist(3,access_playlist_test_three_num+5);
+            cout<<"Test Result: ";
+            if(access_playlist_test_three_result_low == 3 && access_playlist_test_three_result_high == 3){
+                cout<<"PASSED: user was printed an error message and asked to try again. function was then called again"<<endl;
+            }
+            else if (access_playlist_test_three_result_low != 3 && access_playlist_test_three_result_high == 3){
+                cout<<"FAILED: low test failed, high test passed"<<endl;
+            }
+            else if (access_playlist_test_three_result_low == 3 && access_playlist_test_three_result_high != 3){
+                cout<<"FAILED: high test failed, low test passed"<<endl;
+            }
+            else{
+                cout<<"FAILED: undefined behavior"<<endl;
+            }
+            cout<<endl;
+    
     
     
     cout<<"====================="<<endl;
