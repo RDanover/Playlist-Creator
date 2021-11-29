2674db45ca4f1c2afdbc5b8d9649e18c9fb11534
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-29 11:24:41 -0800

    Added unhide_hide function tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -108,6 +108,56 @@ int main()
         cout<<"====================="<<endl;
         cout<<endl;
     
+        cout<<"====================="<<endl;
+        cout<<"RUNNING HIDE_UNHIDE_SONG TESTS"<<endl;
+        cout<<"====================="<<endl;
+        cout<<endl;
+    
+            //Hide unhide song Test One
+            cout<<"Test one: if user responds with LEAVE"<<endl;
+            Public_Playlist* Hide_Unhide_test_one = new Public_Playlist(n)
+            int Hide_Unhide_test_one_result = Hide_Unhide_test_one->hide_unhide_song(1,"LEAVE");
+            cout<<"Test Result: ";
+            if( Hide_Unhide_test_one_result == 1){
+                cout<<"PASSED: user was returned to main menu "<<endl;
+            }
+            else{
+                cout<<"FAILED: undefined behavior"<<endl;
+            }
+            cout<<endl;
+    
+            //Hide unhide song Test Two
+            cout<<"Test two: song is not found"<<endl;
+            Public_Playlist* Hide_Unhide_test_two = new Public_Playlist(n)
+            int Hide_Unhide_test_two_result = Hide_Unhide_test_two->hide_unhide_song(2,"");
+            cout<<"Test Result: ";
+            if( Hide_Unhide_test_two_result == 2 ){
+                cout<<"PASSED: user was printed song not found and asked to try again, function was then called again"<<endl;
+            }
+            else{
+                cout<<"FAILED: undefined behavior"<<endl;
+            }
+            cout<<endl;
+    
+            //Hide unhide song Test Three
+            cout<<"Test three: song was found "<<endl;
+            Public_Playlist* Hide_Unhide_test_three = new Public_Playlist(n)
+            int Hide_Unhide_test_three_result = Hide_Unhide_test_three->hide_unhide_song(3,"");
+            cout<<"Test Result: ";
+            if( Hide_Unhide_test_three_resul == 3){
+                cout<<"PASSED: song was hidden or unhidden based on its previous status and user was sent to the menu"<<endl;
+            }
+            else{
+                cout<<"FAILED: undefined behavior"<<endl;
+            }
+            cout<<endl;
+            
+    
+        cout<<"====================="<<endl;
+        cout<<"FINSIHED HIDE_UNHIDE_SONG TESTS"<<endl;
+        cout<<"====================="<<endl;
+        cout<<endl;
+    
     cout<<endl;
     cout<<"====================="<<endl;
     cout<<"TESTS COMPLETED"<<endl;
