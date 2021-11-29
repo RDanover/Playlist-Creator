a2c4d1525cfade9a836013ec763d7ba240cdfcb1
RDanover <danover.rachel@gmail.com>
2021-11-28 21:50:24 -0800

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+	cout<<"BUGFIXBRANCH"<<endl;
 	cout <<endl;
 	cout << "Welcome to our playlist program. "; 
 	cout << "In this program you will be able to make multiple playlists and add songs to each of them. "; 
