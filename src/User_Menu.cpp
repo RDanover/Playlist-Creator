679e31a20bb58bf3f2b0d2ae6b91601b9994abcd
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-28 21:27:48 -0800

    Update User_Menu.cpp

--- src/User_Menu.cpp
+++ src/User_Menu.cpp
@@ -1,46 +1,84 @@
 #include "../header/User_Menu.hpp"
 #include <iostream>
-void User_Menu::display_options(){
+void User_Menu::display_options(int d){
   std::string option = "B";
-  while (option != "E")
-  {
-     std::cout << endl << "PLAYLIST MENU: ";
-     std::cout << endl << endl;
-     std::cout << "(AD) - ADD NEW PLAYLIST" << std::endl;
-     std::cout << std::endl;
-     std::cout << "(D) - DELETE A PLAYLIST" << std::endl;
-     std::cout << std::endl;
-     std::cout << "(AC) - ACCESS A PLAYLIST" << std::endl;
-     std::cout << std::endl;
-     std::cout << "(E) - EXIT" << std::endl;
-     std::cout << std::endl << std::endl;
-     std::cout << "ENTER OPTION:" << endl;
-     cin >> option;
+  if(d==1){//public
+	while (option != "E")
+  	{
+    		std::cout << endl << "PLAYLIST MENU: ";
+     		std::cout << endl << endl;
+     		std::cout << "(AD) - ADD NEW PLAYLIST" << std::endl;
+    		std::cout << std::endl;
+     		std::cout << "(D) - DELETE A PLAYLIST" << std::endl;
+     		std::cout << std::endl;
+    		std::cout << "(AC) - ACCESS A PLAYLIST" << std::endl;
+     		std::cout << std::endl;
+     		std::cout << "(E) - EXIT" << std::endl;
+     		std::cout << std::endl << std::endl;
+     		std::cout << "ENTER OPTION:" << endl;
+     		cin >> option;
+		cin.ignore();
       
-     if(option == "AD") {
-	add_playlist();
-     }
-     else if(option == "D") {
-	delete_playlist();
-     }
-     else if(option == "AC") {
-	access_playlist();	
-     }
-     else if(option == "E") {
-	return;
-     }
-     else if (option != "E"){
-	  cout << "Invalid input, try again." << endl;
-     }	
+     		if(option == "AD") {
+			public_add_playlist();
+     		}
+     		else if(option == "D") {
+			public_delete_playlist();
+     		}
+     		else if(option == "AC") {
+			public_access_playlist();	
+     		}
+		else if(option == "E") {
+			return;
+     		}
+     		else if (option != "E"){
+	  		cout << "Invalid input, try again." << endl;
+     		}	
 
-   }
+   	}
+  } 
+  else{//private
+	while (option != "E")
+  	{
+     		std::cout << endl << "PLAYLIST MENU: ";
+     		std::cout << endl << endl;
+     		std::cout << "(AD) - ADD NEW PLAYLIST" << std::endl;
+     		std::cout << std::endl;
+     		std::cout << "(D) - DELETE A PLAYLIST" << std::endl;
+    		std::cout << std::endl;
+     		std::cout << "(AC) - ACCESS A PLAYLIST" << std::endl;
+     		std::cout << std::endl;
+     		std::cout << "(E) - EXIT" << std::endl;
+     		std::cout << std::endl << std::endl;
+     		std::cout << "ENTER OPTION:" << endl;
+     		cin >> option;
+		cin.ignore();
+      
+     		if(option == "AD") {
+			private_add_playlist();
+     		}
+     		else if(option == "D") {
+			private_delete_playlist();
+     		}
+     		else if(option == "AC") {
+			private_access_playlist();	
+     		}
+     		else if(option == "E") {
+			return;
+     		}
+     		else if (option != "E"){
+	  		cout << "Invalid input, try again." << endl;
+     		}	
+
+   	}
+  }
+  
 }
 
-Playlist* User_Menu::create_new_playlist(){
-	//insert implementation
+Public_Playlist* User_Menu::public_create_new_playlist(){
 	string playlist_name;
 	cout << "Input the playlist name: ";
-        cin >> playlist_name; // should be changed to getline 
+        getline(cin,playlist_name);
 	
 	while(1){
         	for(int i = 0; i < user_playables.size(); i++) {
@@ -49,13 +87,12 @@ Playlist* User_Menu::create_new_playlist(){
 				return nullptr;
             		}
         	}
-		return new Playlist(playlist_name);
+		return new Public_Playlist(playlist_name);
 	}
-	//user_playables.push_back(new Playlist(playlist_name));
 }
 
-void User_Menu::delete_playlist(){
-for(unsigned int i = 0 ; i < user_playables.size(); i++ ){
+void User_Menu::public_delete_playlist(){
+	for(unsigned int i = 0 ; i < user_playables.size(); i++ ){
 		cout << i+1 << ". " << user_playables.at(i)->get_name() << endl;
     }
     
@@ -66,11 +103,11 @@ for(unsigned int i = 0 ; i < user_playables.size(); i++ ){
     
     if( input < 0 || input > user_playables.size()){
 		cout << "Invalid input please try again"<<endl;
-		delete_playlist();
+		public_delete_playlist();
 	}
 	
 	else if( input == 0) {
-		display_options();
+		return;
 	}
 	
 	else{
@@ -83,18 +120,17 @@ for(unsigned int i = 0 ; i < user_playables.size(); i++ ){
 	}
 }
 
-
 //Function Name: access_playlist()
 //purpose: This function will display all of the users playlists to the user and prompt them for which one they would like to access
 //purpose cont.: if input is correct the user willl be directed to the playlist of their choosing 
 //purpose cont.: if input is not correct user will be asked to try again or be sent back to the menu
-void User_Menu::access_playlist(){
+void User_Menu::public_access_playlist(){
 	if(user_playables.size()==0){
 		cout<<"Please add a playlist before attempting to access a playlist."<<endl;
 		return;
 	}
 	//list playlists starting with a number to index them
-	for( unsigned int i = 0 ; i < user_playables.size(); i++ ){
+	for( unsigned i = 0 ; i < user_playables.size(); i++ ){
 		cout << i+1 << ". " << user_playables.at(i)->get_name() << endl;
         }
 
@@ -106,10 +142,10 @@ void User_Menu::access_playlist(){
 	//check input then send user to the correct playlist
 	if( input < 0 || input > user_playables.size()){
 		cout << "Invalid input please try again"<<endl;
-		access_playlist();
+		public_access_playlist();
 	}
 	else if( input == 0){
-		display_options();
+		return;
 	}
 	else{
 		user_playables.at(input-1)->display();
@@ -117,20 +153,108 @@ void User_Menu::access_playlist(){
 
 }
 
-void User_Menu::add_playlist()
+void User_Menu::public_add_playlist()
 {	
 	//string playlist_name;
 	//cout << "Input the playlist name: ";
 	//getline(cin, playlist_name);
-	Playlist* new_playlist = create_new_playlist();
+	Public_Playlist* new_playlist = public_create_new_playlist();
 	if(new_playlist)
 	{
 		user_playables.push_back(new_playlist);
 	}
 	
 }
+//END PUBLIC FUNCTIONS
 
-void User_Menu::exit(){
-	//insert implementation
-	return;
+//START PRIVATE FUNCTIONS	
+Private_Playlist* User_Menu::private_create_new_playlist(){
+	string playlist_name;
+	cout << "Input the playlist name: ";
+        getline(cin,playlist_name);
+	
+	while(1){
+        	for(int i = 0; i < user_playables.size(); i++) {
+            		if(user_playables.at(i)->get_name() == playlist_name) {
+               	 		cout << "the name already exists!" << endl;
+				return nullptr;
+            		}
+        	}
+		return new Private_Playlist(playlist_name);
+	}
+}
+
+void User_Menu::private_delete_playlist(){
+	for(unsigned int i = 0 ; i < user_playables.size(); i++ ){
+		cout << i+1 << ". " << user_playables.at(i)->get_name() << endl;
+    }
+    
+    int input = 0;
+    cout << "Please type the number next to the playlist you would like to delete, or to return to options press 0 and enter" << endl;
+    cin >> input;
+    cin.ignore();
+    
+    if( input < 0 || input > user_playables.size()){
+		cout << "Invalid input please try again"<<endl;
+		private_delete_playlist();
+	}
+	
+	else if( input == 0) {
+		return;
+	}
+	
+	else{
+		string n = user_playables.at(input-1)->get_name();
+		//for(unsigned int i = 0; i < user_playables.at(input-1)->get_length(); i++){
+			user_playables.at(input-1)->deleteP();
+		//	}
+			delete user_playables.at(input-1);
+		cout << n << " was deleted" << endl; 
+	}
+}
+
+//Function Name: access_playlist()
+//purpose: This function will display all of the users playlists to the user and prompt them for which one they would like to access
+//purpose cont.: if input is correct the user willl be directed to the playlist of their choosing 
+//purpose cont.: if input is not correct user will be asked to try again or be sent back to the menu
+void User_Menu::private_access_playlist(){
+	if(user_playables.size()==0){
+		cout<<"Please add a playlist before attempting to access a playlist."<<endl;
+		return;
+	}
+	//list playlists starting with a number to index them
+	for( unsigned i = 0 ; i < user_playables.size(); i++ ){
+		cout << i+1 << ". " << user_playables.at(i)->get_name() << endl;
+        }
+
+	//ask for user input
+	int input = 0;
+	cout << "Please type the number next to the playlist you would like to access and hit enter, or to return to options press 0 and hit enter" << endl;
+	cin >> input;	
+	cin.ignore();//clears the newline left in th stream incase getline is used after this
+	//check input then send user to the correct playlist
+	if( input < 0 || input > user_playables.size()){
+		cout << "Invalid input please try again"<<endl;
+		private_access_playlist();
+	}
+	else if( input == 0){
+		return;
+	}
+	else{
+		user_playables.at(input-1)->display();
+	}
+
+}
+
+void User_Menu::private_add_playlist()
+{	
+	//string playlist_name;
+	//cout << "Input the playlist name: ";
+	//getline(cin, playlist_name);
+	Private_Playlist* new_playlist = private_create_new_playlist();
+	if(new_playlist)
+	{
+		user_playables.push_back(new_playlist);
+	}
+	
 }
